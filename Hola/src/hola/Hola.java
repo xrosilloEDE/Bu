f9e0544c6c4334c6a0c4ae3edf75a2c01ba9c185
rosillo <rosillo_xav@gva.es>
2021-01-26 19:38:15 +0100

    uno

---
+++ Hola/src/hola/Hola.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hola;
+
+/**
+ * @author xrosillo
+ *
+ */
+public class Hola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		System.out.println("Adios");
+
+	}
+
+}
